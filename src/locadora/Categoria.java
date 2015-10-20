73207cbbb7a7b0fe9d1c4052cf4edae6feee74fd
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-10-20 19:18:31 -0200

    Movendo para Enum...

--- src/locadora/Categoria.java
+++ src/locadora/Categoria.java
@@ -1,7 +1,24 @@
 package locadora;
 
+import locadora.preco.Preco;
+import locadora.preco.PrecoInfantil;
+import locadora.preco.PrecoLancamento;
+import locadora.preco.PrecoNormal;
+
 public enum Categoria {
-	NORMAL,
-	LANCAMENTO,
-	INFANTIL
+	NORMAL(new PrecoNormal()),
+	LANCAMENTO(new PrecoLancamento()),
+	INFANTIL(new PrecoInfantil());
+	
+	private final Preco preco;
+	
+	private Categoria(Preco preco) {
+		this.preco = preco;
+	}
+
+	public Preco getPreco() {
+		return preco;
+	}
+	
+	
 }
--- src/locadora/Filme.java
+++ src/locadora/Filme.java
@@ -1,10 +1,5 @@
 package locadora;
 
-import locadora.preco.Preco;
-import locadora.preco.PrecoInfantil;
-import locadora.preco.PrecoLancamento;
-import locadora.preco.PrecoNormal;
-
 public class Filme {
 
 	private String titulo;
@@ -24,26 +19,7 @@ public class Filme {
 		this.categoria = categoria;
 	}
 	public double calculaValorLocacao(int diasDeAluguel) {
-		Preco preco = null;
-		switch (getCategoria()) {
-	
-		case NORMAL:
-			preco = new PrecoNormal();
-			break;
-			
-		case LANCAMENTO:
-			preco = new PrecoLancamento();
-			break;
-			
-		case INFANTIL:
-			preco = new PrecoInfantil();
-			break;
-			
-		default:
-			throw new RuntimeException("Categoria Inválida");
-
-		}
-		return preco.calcula(diasDeAluguel);
+		return categoria.getPreco().calcula(diasDeAluguel);
 	}
 
 }
